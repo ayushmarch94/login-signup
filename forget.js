45bab0c890ba531fc2d6665771648da5605b4bd6
AYUSH KUMAR <125579641+ayushmarch94@users.noreply.github.com>
2024-06-12 10:32:07 +0530

    Updated OTP system

---
+++ forget.js
@@ -0,0 +1,36 @@
+function countdown() {
+  let timeLeft = 60;
+  const countdown = document.getElementById("countdown");
+  const timer = setInterval(() => {
+    timeLeft--;
+    countdown.textContent = timeLeft + "s";
+
+    if (timeLeft <= 0) {
+      clearInterval(timer);
+      countdown.textContent = "Please reload your page";
+    }
+  }, 1000);
+}
+countdown();
+
+document.getElementById("signup-form").addEventListener("submit", function (event) {
+    event.preventDefault();
+    window.location.href = "./land.html";
+});
+
+
+
+function generateOTP(length) {
+  let otp = '';
+  const characters = '0123456789';
+  for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * characters.length);
+      otp += characters[randomIndex];
+  }
+  window.alert("your OTP is "+otp);
+  return otp;
+}
+
+generateOTP(6)
+
+
