fce58adcdb6cfb14b853dea86fa0bf940f167633
AYUSH KUMAR <125579641+ayushmarch94@users.noreply.github.com>
2024-06-13 17:48:21 +0530

    Created forget password option (access the password from database)

--- forget.php
+++ forget.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,21 +8,39 @@
     <link rel="icon" href="./check.png">
     <link rel="stylesheet" href="./styles.css">
 </head>
+
 <body>
     <div class="signup">
         <div class="signup-form">
             <div class="heading">
-                <h1>Reset Password</h1>
+                <h1>Get your Password</h1>
             </div>
-            <form id="signup-form" action="/forget">
-                <input required placeholder="Enter OTP">
-                <input required placeholder="Enter new password">
-                <p>OTP is sent to your email</p>
-                <p id="countdown">60s</p>
-                <button type="submit">Ok</button>
+            <form id="signup-form" action="forget.php" method="POST">
+                <input type="email" required name="EMAIL" placeholder="Enter Email">
+                <button type="submit">Get password on Email</button>
             </form>
         </div>
     </div>
-    <script src="./forget.js"></script>
 </body>
+
 </html>
+
+<?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        include 'db.php';
+        $email = $_POST['EMAIL'];
+        $sql = "SELECT PASSWORD FROM data WHERE EMAIL = '$email'";
+        $result = mysqli_query($con, $sql);
+        $row = mysqli_fetch_assoc($result);
+        echo "Password " . $row["PASSWORD"];   
+        mysqli_close($con);
+    }
+?>
+
+
+
+
+
+
+
+
